03fa0bb64cd0b31de4efaa50d819201f03392fc2
Serge Zaitsev <serge.zaitsev@grafana.com>
2022-01-28 14:50:29 +0100

    remove more dispatches

--- pkg/services/login/authinfo.go
+++ pkg/services/login/authinfo.go
@@ -9,4 +9,5 @@ import (
 type AuthInfoService interface {
 	LookupAndUpdate(ctx context.Context, query *models.GetUserByAuthInfoQuery) (*models.User, error)
 	UpdateAuthInfo(ctx context.Context, cmd *models.UpdateAuthInfoCommand) error
+	SetAuthInfo(ctx context.Context, cmd *models.SetAuthInfoCommand) error
 }
--- pkg/services/sqlstore/user.go
+++ pkg/services/sqlstore/user.go
@@ -30,7 +30,7 @@ func (ss *SQLStore) addUserQueryAndCommandHandlers() {
 	bus.AddHandler("sql", ss.GetUserProfile)
 	bus.AddHandler("sql", SearchUsers)
 	bus.AddHandler("sql", ss.GetUserOrgList)
-	bus.AddHandler("sql", DisableUser)
+	bus.AddHandler("sql", ss.DisableUser)
 	bus.AddHandler("sql", ss.BatchDisableUsers)
 	bus.AddHandler("sql", ss.DeleteUser)
 	bus.AddHandler("sql", ss.SetUserHelpFlag)
@@ -721,7 +721,7 @@ func SearchUsers(ctx context.Context, query *models.SearchUsersQuery) error {
 	return err
 }
 
-func DisableUser(ctx context.Context, cmd *models.DisableUserCommand) error {
+func (ss *SQLStore) DisableUser(ctx context.Context, cmd *models.DisableUserCommand) error {
 	user := models.User{}
 	sess := x.Table("user")
 
