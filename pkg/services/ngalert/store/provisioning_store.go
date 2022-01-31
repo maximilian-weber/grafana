d5d6e3372c54dc0708c721436aaef354d37056c8
Alex Weaver <weaver.alex.d@gmail.com>
2022-01-31 16:11:48 -0600

    Drop completed TODO

--- pkg/services/ngalert/store/provisioning_store.go
+++ pkg/services/ngalert/store/provisioning_store.go
@@ -69,7 +69,6 @@ func (st DBstore) SetProvenanceTransactional(o models.ProvisionedObject, p model
 	recordKey := o.GetResourceUniqueIdentifier()
 
 	uow = uow.Do(func(sess *sqlstore.DBSession) error {
-		// TODO: Add a unit-of-work pattern, so updating objects + provenance will happen consistently with rollbacks across stores.
 		// TODO: Need to make sure that writing a record where our concurrency key fails will also fail the whole transaction. That way, this gets rolled back too. can't just check that 0 updates happened inmemory. Check with jp. If not possible, we need our own concurrency key.
 		// TODO: Clean up stale provenance records periodically.
 		q := "DELETE FROM provenance_type WHERE record_key = ? AND record_type = ?"
